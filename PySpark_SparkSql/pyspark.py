606a051ad3c80af115cb2517ca715693dc31f353
Hemanth Kumar <digitalhemanth@gmail.com>
2022-01-17 00:00:38 +0530

    Update pyspark.py

--- PySpark_SparkSql/pyspark.py
+++ PySpark_SparkSql/pyspark.py
@@ -1,11 +1,15 @@
+import findspark
+findspark.init()
+
 import pyspark
+from pyspark import SparkConf,SparkContext
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
-sparkContext=spark.sparkContext
+conf = pyspark.SparkConf().setAppName('MyApp').setMaster('local')
+sc = pyspark.SparkContext(conf=conf)
+spark = SparkSession(sc)
 
-
-rdd=sparkContext.parallelize([1,2,3,4,5])
+rdd=spark.parallelize([1,2,3,4,5])
 rddCollect = rdd.collect()
 print("Number of Partitions: "+str(rdd.getNumPartitions()))
 print("Action: First element: "+str(rdd.first()))
