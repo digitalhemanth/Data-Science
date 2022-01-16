5f111e9a8030575dbb3c4bd2c5c718eaf2724270
Hemanth Kumar <digitalhemanth@gmail.com>
2022-01-16 23:53:56 +0530

    conne

--- PySpark_SparkSql/pyspark.py
+++ PySpark_SparkSql/pyspark.py
@@ -3,3 +3,11 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 sparkContext=spark.sparkContext
+
+
+rdd=sparkContext.parallelize([1,2,3,4,5])
+rddCollect = rdd.collect()
+print("Number of Partitions: "+str(rdd.getNumPartitions()))
+print("Action: First element: "+str(rdd.first()))
+print(rddCollect)
+
