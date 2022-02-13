8512cc281c8d1a701905a26c204c338a275a9c4b
Hemanth Kumar <digitalhemanth@gmail.com>
2022-02-13 12:50:14 +0530

    Update Spark_interview.py

--- PySpark_SparkSql/Spark_interview.py
+++ PySpark_SparkSql/Spark_interview.py
@@ -203,3 +203,28 @@ rdd2.saveAsTextFile("c://tmp/re-partition2")
 rdd3 = rdd1.coalesce(4)
 print("Repartition size : "+str(rdd3.getNumPartitions()))
 rdd3.saveAsTextFile("c:/tmp/coalesce2")
+
+
+
+
+structureData = [
+    (("James","","Smith"),"36636","M",3100),
+    (("Michael","Rose",""),"40288","M",4300),
+    (("Robert","","Williams"),"42114","M",1400),
+    (("Maria","Anne","Jones"),"39192","F",5500),
+    (("Jen","Mary","Brown"),"","F",-1)
+  ]
+structureSchema = StructType([
+        StructField('name', StructType([
+             StructField('firstname', StringType(), True),
+             StructField('middlename', StringType(), True),
+             StructField('lastname', StringType(), True)
+             ])),
+         StructField('id', StringType(), True),
+         StructField('gender', StringType(), True),
+         StructField('salary', IntegerType(), True)
+         ])
+
+df2 = spark.createDataFrame(data=structureData,schema=structureSchema)
+df2.printSchema()
+df2.show(truncate=False)
