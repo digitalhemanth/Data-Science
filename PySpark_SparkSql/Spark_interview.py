1de30edffdfc6380c17feec465db95a872758dd0
Hemanth Kumar <digitalhemanth@gmail.com>
2022-02-13 12:33:22 +0530

    Update Spark_interview.py

--- PySpark_SparkSql/Spark_interview.py
+++ PySpark_SparkSql/Spark_interview.py
@@ -88,3 +88,89 @@ df.select(sum("salary")).show(truncate=False)
 df.select(sumDistinct("salary")).show(truncate=False)
 df.select(variance("salary"),var_samp("salary"),var_pop("salary")) \
   .show(truncate=False)
+
+
+##################################################################################################
+
+
+empDF.join(deptDF,empDF.emp_dept_id ==  deptDF.dept_id,"inner") \
+     .show(truncate=False)
+
+
+empDF.join(deptDF,empDF.emp_dept_id ==  deptDF.dept_id,"outer") \
+    .show(truncate=False)
+empDF.join(deptDF,empDF.emp_dept_id ==  deptDF.dept_id,"full") \
+    .show(truncate=False)
+empDF.join(deptDF,empDF.emp_dept_id ==  deptDF.dept_id,"fullouter") \
+    .show(truncate=False)
+
+
+
+
+  empDF.join(deptDF,empDF("emp_dept_id") ==  deptDF("dept_id"),"left")
+    .show(false)
+  empDF.join(deptDF,empDF("emp_dept_id") ==  deptDF("dept_id"),"leftouter")
+    .show(false)
+
+
+
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, ArrayType
+from pyspark.sql.functions import col,array_contains
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+arrayStructureData = [
+        (("James","","Smith"),["Java","Scala","C++"],"OH","M"),
+        (("Anna","Rose",""),["Spark","Java","C++"],"NY","F"),
+        (("Julia","","Williams"),["CSharp","VB"],"OH","F"),
+        (("Maria","Anne","Jones"),["CSharp","VB"],"NY","M"),
+        (("Jen","Mary","Brown"),["CSharp","VB"],"NY","M"),
+        (("Mike","Mary","Williams"),["Python","VB"],"OH","M")
+        ]
+        
+arrayStructureSchema = StructType([
+        StructField('name', StructType([
+             StructField('firstname', StringType(), True),
+             StructField('middlename', StringType(), True),
+             StructField('lastname', StringType(), True)
+             ])),
+         StructField('languages', ArrayType(StringType()), True),
+         StructField('state', StringType(), True),
+         StructField('gender', StringType(), True)
+         ])
+
+
+df = spark.createDataFrame(data = arrayStructureData, schema = arrayStructureSchema)
+df.printSchema()
+df.show(truncate=False)
+
+df.filter(df.state == "OH") \
+    .show(truncate=False)
+
+df.filter(col("state") == "OH") \
+    .show(truncate=False)    
+    
+df.filter("gender  == 'M'") \
+    .show(truncate=False)    
+
+df.filter( (df.state  == "OH") & (df.gender  == "M") ) \
+    .show(truncate=False)        
+
+df.filter(array_contains(df.languages,"Java")) \
+    .show(truncate=False)        
+
+df.filter(df.name.lastname == "Williams") \
+    .show(truncate=False) 
+
+
+
+df.createOrReplaceTempView("PERSON_DATA")
+df2 = spark.sql("SELECT * from PERSON_DATA")
+df2.printSchema()
+df2.show()
+
+
+
