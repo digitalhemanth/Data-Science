6c2435c4a99bb1391ef8a699e03096fff150b61f
Hemanth Kumar <digitalhemanth@gmail.com>
2022-11-16 23:57:00 +0530

    Update mask.sql

--- mask.sql
+++ mask.sql
@@ -4,5 +4,6 @@ COMMENT ON ROLE Normal_user IS 'MASKED';
 
 CREATE TABLE pytest.CUST_TABLE ( id TEXT, firstname TEXT, lastname TEXT, phone TEXT, card_no TEXT);
 
+INSERT INTO pytest.CUST_TABLE VALUES ('001','Hemanth', 'Kumar','9966060174','0609110911');
 
 COMMENT ON COLUMN patient.name IS 'MASKED WITH FUNCTION anon.random_name()';
