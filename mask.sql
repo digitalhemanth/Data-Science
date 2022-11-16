31ec723a2945aa0189ed9e17dfe78111923eba52
Hemanth Kumar <digitalhemanth@gmail.com>
2022-11-17 00:06:02 +0530

    Update mask.sql

--- mask.sql
+++ mask.sql
@@ -6,4 +6,6 @@ CREATE TABLE pytest.CUST_TABLE ( id TEXT, firstname TEXT, lastname TEXT, phone T
 
 INSERT INTO pytest.CUST_TABLE VALUES ('001','Hemanth', 'Kumar','9966060174','0609110911');
 
+select * from pytest.CUST_TABLE;
+
 COMMENT ON COLUMN patient.name IS 'MASKED WITH FUNCTION anon.random_name()';
